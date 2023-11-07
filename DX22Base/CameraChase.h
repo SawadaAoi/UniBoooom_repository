3088f11a0b7bf5e74451a4d3f6921addba3604f5
TakagiShunsuke <shunsuke.tsum@gmail.com>
2023-11-07 12:08:57 +0900

    カメラ修正

--- DX22Base/CameraChase.h
+++ DX22Base/CameraChase.h
@@ -9,7 +9,8 @@
 
 	変更履歴
 	・2023/11/04 制作 takagi
-	・2023/11/06 コーディング規約適用・追跡対象登録機構作成
+	・2023/11/06 コーディング規約適用・追跡対象登録機構作成 takagi
+	・2023/11/07 GetViewMatrix()関数にconst修飾子付与・コメント修正 takagi
 
 ========================================== */
 
@@ -27,7 +28,7 @@ public:
 	CCameraChase(const TPos<float>* pPos);			//コンストラクタ
 	~CCameraChase();								//デストラクタ
 	void Update();									//更新
-	DirectX::XMFLOAT4X4 GetViewMatrix() override;	//ビュー行列変換
+	DirectX::XMFLOAT4X4 GetViewMatrix() const override;	//ビュー行列変換
 private:
 	// ===メンバ変数宣言===
 	const TPos<float>* m_pTarget;	//追跡目標
