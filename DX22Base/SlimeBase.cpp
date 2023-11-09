532475ac7daf5015a931a6ca4f0a47bf2cbc4db0
SawadaAoi <misokatsu2234@icloud.com>
2023-11-09 17:36:25 +0900

    プレイヤー追跡移動処理の範囲外の処理を追加

--- DX22Base/SlimeBase.cpp
+++ DX22Base/SlimeBase.cpp
@@ -15,6 +15,7 @@
 	・2023/11/08 GetPos→GetSphereに名前を変更 / 山下凌佑
 	・2023/11/08 定数定義がヘッダーにあったのでcppに移動 / 山下凌佑
 	・2023/11/08 コメントを追加　澤田蒼生
+	・2023/11/09 プレイヤー追跡移動変更
 
 ========================================== */
 
@@ -27,7 +28,7 @@ const float ENEMY_MOVE_SPEED = 0.01f;
 const float SPEED_DOWN_RATIO = 0.6f;	//スライムが接触して吹き飛ぶ際にかかる移動速度の変化の割合	RATIO=>割合
 const float MOVE_RESIST = 0.1f;		//吹き飛び移動中のスライムの移動速度に毎フレームかかる減算数値
 const float REFLECT_RATIO = 0.1f;	//スライムがスライムを吹き飛ばした際に吹き飛ばした側のスライムの移動量を変える割合
-const float MOVE_DISTANCE_PLAYER = 5;
+const float MOVE_DISTANCE_PLAYER = 20;	// プレイヤー追跡移動に切り替える距離
 
 /* ========================================
 	コンストラクタ関数
@@ -193,6 +194,12 @@ void CSlimeBase::NormalMove(CSphereInfo::Sphere playerSphere)
 			m_move.x = movePos.x / distancePlayer * m_fSpeed;
 			m_move.z = movePos.z / distancePlayer * m_fSpeed;
 		}
+	}
+	else
+	{
+		m_move.x = 0;
+		m_move.z = 0;
+
 	}
 
 }
