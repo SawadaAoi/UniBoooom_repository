c99795835cb89335cddb2685942400a2aa22dbe8
SawadaAoi2 <misokatsu2234@icloud.com>
2023-12-01 05:30:08 +0900

    HPアニメーションが正常に動作するように修正

--- DX22Base/HP_UI.cpp
+++ DX22Base/HP_UI.cpp
@@ -243,7 +243,7 @@ void CHP_UI::Draw()
 		DirectX::XMFLOAT4X4 mat_Anim[3];
 
 		// ワールド行列はXとYのみを考慮して作成
-		DirectX::XMMATRIX world = DirectX::XMMatrixTranslation(HP_UI_SIZE.x + (m_nPlayerOldHp * DRAW_GAP), DRAW_POSY, 0.0f);	// ワールド行列（必要に応じて変数を増やしたり、複数処理を記述したりする）
+		DirectX::XMMATRIX world = DirectX::XMMatrixTranslation(HP_UI_POS.x + (hpCnt * DRAW_WIDTH), HP_UI_POS.y, 0.0f);	// ワールド行列（必要に応じて変数を増やしたり、複数処理を記述したりする）
 		DirectX::XMStoreFloat4x4(&mat_Anim[0], DirectX::XMMatrixTranspose(world));
 
 		// ビュー行列は2Dだとカメラの位置があまり関係ないので、単位行列を設定する
@@ -262,7 +262,7 @@ void CHP_UI::Draw()
 		Sprite::SetSize(DirectX::XMFLOAT2(DRAW_ANIM_HEIGHT, -DRAW_ANIM_WIDTH));
 		Sprite::SetUVPos(DirectX::XMFLOAT2(m_fUVposX, m_fUVposY));				
 		Sprite::SetUVScale(DirectX::XMFLOAT2(HP_ANIM_SIZEX, HP_ANIM_SIZEY));
-		if (*m_pPlayerHp % 2 == 1)	// HPが偶数か奇数かによって表示するテクスチャを変更する
+		if (*m_pPlayerHp % 2 == 0)	// HPが偶数か奇数かによって表示するテクスチャを変更する
 		{
 			Sprite::SetTexture(m_pTexture[HEART_HE_ANIM]);
 		}
