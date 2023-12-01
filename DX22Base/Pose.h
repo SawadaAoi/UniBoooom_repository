f1410ae54c82b0ae22c7fc6e0af8aa9cefd25902
TakagiShunsuke <shunsuke.tsum@gmail.com>
2023-12-01 17:35:30 +0900

    過去に別ブランチで作ったシェーダーを持ってくるためのコミット

---
+++ DX22Base/Pose.h
@@ -0,0 +1,44 @@
+/* ========================================
+	HEW/UniBoooom!!
+	---------------------------------------
+	ポーズ画面定義
+	---------------------------------------
+	Pose.h
+
+	作成者	takagi
+
+	変更履歴
+	・2023/12/01 制作 takagi
+
+========================================== */
+
+#ifndef __RESULT_H__	//Pose.hインクルードガード
+#define __RESULT_H__
+
+#include "Sound.h"		//音
+#include "Camera.h"		//カメラ
+#include "2dPolygon.h"	//平面ポリゴン
+#include <vector>		//配列コンテナ
+
+// =============== クラス定義 =====================
+class CPose
+{
+public:
+	// ===プロトタイプ宣言===
+	CPose(CCamera* pCamera);	//コンストラクタ
+	~CPose();					//デストラクタ
+	void Update();				//更新
+	void Draw();				//描画
+	bool IsFin() const;			//終了確認
+protected:
+	// ===メンバ変数宣言=====
+	bool m_bFinish;									//終了予約用(trueで終了)
+	std::vector<C2dPolygon*> m_2dObj;				//平面ポリゴン
+	CCamera* m_pCamera;								//カメラ
+	XAUDIO2_BUFFER* m_pBGM;							//BGMの音声データ
+	XAUDIO2_BUFFER* m_pSEHitHammer;					//SEの音声データ
+	IXAudio2SourceVoice* m_pSpeaker;				//BGMを聞き取る側
+	IXAudio2SourceVoice* m_pSEHitHammerSpeaker;		//SEを聞き取る側
+};	//リザルト
+
+#endif	//!__RESULT_H__
